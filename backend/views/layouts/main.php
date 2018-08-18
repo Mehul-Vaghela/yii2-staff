1ab904d0322e7c19b26da771440ed10bbead8c30
Mehul Vaghela <vaghelamehul12345@gmail.com>
2018-08-18 10:01:14 +0530

    minor change

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -30,6 +30,7 @@ AppAsset::register($this);
         <meta charset="<?= Yii::$app->charset ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="Y9VPCuJCKWiRCQWH7qncJBcUcoMd-2PzzIoYmB4-4R4" />
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
